d0509bb81c9157a99e599672f65a5303d8715bdc
SebastianRG16 <89326030+SebastianRG16@users.noreply.github.com>
2023-11-07 18:04:00 -0500

    commit ultimo

--- cita/api/serializers.py
+++ cita/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from cita.models import Cita
+from paciente.api.serializers import PacienteSerializer
+from consultorio.api.serializers import ConsultorioSerializer
+from practicante.api.serializers import PracticanteSerializer
 
 class CitaSerializer(serializers.ModelSerializer):
+    id_paciente = PacienteSerializer(read_only=True)
+    id_consultorio = ConsultorioSerializer(read_only=True)
+    id_practicante = PracticanteSerializer(read_only=True)
     class Meta:
         model = Cita
-        fields = '__all__'
+        fields = ['id','name','id_paciente', 'id_consultorio', 'id_practicante', 'date', 'state']
